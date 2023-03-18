8eaadbadba7d0d910eb5058df002cae6068f2e47
petitbon <petitbon@gmail.com>
2023-03-18 16:58:41 -0400

    feat: feedback to model roughly implemented.

---
+++ functions/http-models-feedback/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const functions = require("@google-cloud/functions-framework");
+const firebase = require("firebase-admin");
+
+firebase.initializeApp();
+
+functions.http("modelsFeedback", async (req, res) => {
+  const body = {
+    model: req.body.model,
+    prompt: req.body.prompt,
+    completion: req.body.completion,
+  };
+
+  if (req.method === "POST") {
+    switch (req.get("content-type")) {
+      case "application/json":
+        const firestore = firebase.firestore();
+        await firestore.collection("models_feedback").add(body);
+        res.status(200).send("OK");
+        break;
+    }
+  }
+});
