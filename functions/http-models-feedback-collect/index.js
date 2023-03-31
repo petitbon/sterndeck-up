5e189c4782811c9c10cda342cee883be43d9d274
petitbon <petitbon@gmail.com>
2023-03-31 13:54:50 -0400

    working on api keys / sdk for openapi specs

--- functions/http-models-feedback-collect/index.js
+++ functions/http-models-feedback-collect/index.js
@@ -2,23 +2,82 @@
 
 const functions = require("@google-cloud/functions-framework");
 const firebase = require("firebase-admin");
+const { Storage } = require("@google-cloud/storage");
+const csvWriter = require("csv-writer").createObjectCsvWriter;
 
 firebase.initializeApp();
+const storage = new Storage();
+const firestore = firebase.firestore();
 
-functions.http("modelsFeedbackCollect", async (req, res) => {
+const myPID = process.env.PROJECT_NAME || "sterndeck-4";
+
+if (!myPID) {
+  res.status(500).send(`Environment variables are not set.`);
+  return;
+}
+
+functions.http("feedbackCollect", async (req, res) => {
   const body = {
-    model: req.body.model,
-    prompt: req.body.prompt,
-    completion: req.body.completion,
+    threshold: req.body.threshold || 200,
   };
 
   if (req.method === "POST") {
     switch (req.get("content-type")) {
       case "application/json":
-        const firestore = firebase.firestore();
-        await firestore.collection("models_feedback").add(body);
+        await countItemsAndStore(body.threshold);
         res.status(200).send("OK");
         break;
     }
   }
 });
+
+async function countItemsAndStore(threshold) {
+  const collectionRef = firestore.collection("models_feedback");
+  const querySnapshot = await collectionRef.get();
+
+  const modelCounts = new Map();
+
+  querySnapshot.docs.forEach((doc) => {
+    const model = doc.data().model;
+    const count = modelCounts.get(model) || 0;
+    modelCounts.set(model, count + 1);
+  });
+
+  for (const [model, count] of modelCounts) {
+    if (count >= threshold) {
+      const items = querySnapshot.docs
+        .filter((doc) => doc.data().model === model)
+        .map((doc) => {
+          const data = doc.data();
+          return { prompt: data.prompt, completion: data.completion };
+        });
+
+      const bucketName = `${myPID}.appspot.com`;
+      const fileName = Date.now() + ".csv";
+
+      await storeItemsAsCsv(items, fileName, bucketName, model);
+    }
+    //console.log(`Count of items with model "${model}": ${count}`);
+  }
+}
+
+async function storeItemsAsCsv(items, csvPath, bucketName, model) {
+  const header = [
+    { id: "prompt", title: "prompt" },
+    { id: "completion", title: "completion" },
+  ];
+
+  const writer = csvWriter({
+    path: csvPath,
+    header,
+  });
+
+  await writer.writeRecords(items);
+  const options = {
+    destination: "feeback-files/" + model + "/" + csvPath,
+  };
+
+  const bucket = storage.bucket(bucketName);
+  await bucket.upload(csvPath, options);
+  //console.log(`CSV file stored at: gs://${bucketName}/${csvPath}`);
+}
